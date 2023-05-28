01beb47dd7ff3fc7fec1165157dd7d512f751ae3
Alex Mills <alexmills@Alexs-MacBook-Air.local>
2023-05-27 23:53:21 -0400

    Adding wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local colors = require('lua/rose-pine').colors()
+local window_frame = require('lua/rose-pine').window_frame()
+
+return {
+  window_decorations = "RESIZE",
+  color_scheme ="rose-pine",
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  use_cap_height_to_scale_fallback_fonts = true,
+  font_size = 16,
+  scrollback_lines = 5000,
+  use_fancy_tab_bar = true,
+  colors = colors,
+  window_frame = window_frame,
+}
